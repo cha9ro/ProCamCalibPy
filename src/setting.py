40451107a7de8224d46e8cc69dca01f7c805540e
cha9ro <t910inicident@gmail.com>
2018-05-28 17:39:14 +0900

    everything

---
+++ src/setting.py
@@ -0,0 +1,14 @@
+# The number of images you already took
+IMG_NUM = 0
+
+# Camera screen size
+CAM_WIDTH = 1280
+CAM_HEIGHT = 960
+
+# Projector screen size
+PRO_WIDTH = 1920
+PRO_HEIGHT = 1080
+
+# Projection window position
+WIN_OX = 0 #3840 + 1200
+WIN_OY = 0
